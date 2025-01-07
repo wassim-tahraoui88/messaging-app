8400bb5717ef1aac741d5f13f0aff4009587ba78
Wassim Tahraoui <wassim.tahraoui@horizon-tech.tn>
2025-01-07 03:47:17 +0100

    - Home Page - Network Connection Backend Setup

--- src/main/java/com/tahraoui/messaging/backend/ConnectionService.java
+++ src/main/java/com/tahraoui/messaging/backend/ConnectionService.java
@@ -2,13 +2,17 @@ package com.tahraoui.messaging.backend;
 
 import com.tahraoui.messaging.backend.client.Client;
 import com.tahraoui.messaging.backend.host.Host;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 
 public class ConnectionService {
+	private static final Logger LOGGER = LogManager.getLogger(ConnectionService.class.getName());
 
 	public static void host(int port, String password) {
 		var host = new Host(port, password);
+		LOGGER.debug("Host started on port {}", port);
 
 	}
 	public static void join(int port, String password) {
